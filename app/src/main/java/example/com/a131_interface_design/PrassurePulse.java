7181772d7aae8dc1f08e3d8a49dce6607500f479
Алексей <avdeev.hexer@yandex.ru>
2020-07-24 09:28:54 +0300

    Optimisation Date for Activity PressurePulse

--- app/src/main/java/example/com/a131_interface_design/PrassurePulse.java
+++ app/src/main/java/example/com/a131_interface_design/PrassurePulse.java
@@ -3,8 +3,13 @@ package example.com.a131_interface_design;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.util.LocaleData;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PrassurePulse extends AppCompatActivity {
 
@@ -12,6 +17,11 @@ public class PrassurePulse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prassure_pulse);
+        TextView txtdateTime = findViewById(R.id.actPressureTxtDateTimeNow);
+        //Date dateTimeNow = new Date();
+        //SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy ' ' hh:mm");
+        txtdateTime.setText(new SimpleDateFormat("dd.MM.yyyy ' ' hh:mm").format(new Date()));
+
     }
 
     public void btnSaveClicked (View view) {
